826a45b205805fde7c4dae0513b308c2c9e7f9a2
yusufaverroes <yusuf.averoes@gmail.com>
2024-11-06 13:56:58 +0700

    insyaa allah bener

--- utils/printerTemplates.js
+++ utils/printerTemplates.js
@@ -37,8 +37,7 @@ const printerTemplate = {
         
         const QRModule = ["0301"];// buffer's field 1
         // const BPOM = printer.createModuleText("       BPOM RI      ",  true, 10 ,3,  0,  17,  0,  "Arial");
-        const BPOM = printer.createModuleText("      BPOM RI       ",  true, 10 ,3,  0,  17,  0,  "Arial");
-        const QR = printer.createModuleQR(QRModule,  10,  50, 2,  0, 'Data Matrix', 'H',  10, false,  'Blank', 0);
+        const QR = printer.createModuleQR(QRModule,  10,  18, 4,  0, 'Data Matrix', 'H',  10, false,  'Blank', 0);
         const BN = printer.createModuleText(`BN  ${details.BN}`,  true,  10,  154+3,  0,  22,  0,  "Arial"); 
         const MD = printer.createModuleText(`MD  ${details.MD}`,  true,  10,  178+3,  0,  22,  0,  "Arial");
         const ED = printer.createModuleText(`ED  ${details.ED}`,  true,  10,  206+3,  0,  22,  0,  "Arial");
@@ -47,7 +46,7 @@ const printerTemplate = {
         //const modules= [QR, BPOM,field,BN, MD, ED, HET]
         const modules= [ BPOM,QR, field,BN, MD, ED, HET]
         const msg = printer.createMsg(modules, fileName);
-        console.log("[PrinterTamplate] template3 is chosen")
+        console.log("[PrinterTamplate] tamplate3 is chosen")
         return msg;
     }
 };
